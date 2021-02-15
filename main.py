f81cf25d62eb9fa6fcc73eecac08b6a3916ca0b2
iTrauco <chris@trau.co>
2021-02-15 16:18:34 -0500

    pygsheets formatting w/ gspread

--- main.py
+++ main.py
@@ -6,18 +6,29 @@ import pygsheets
 
 gc = gspread.service_account('credentials.json')
 
+client = pygsheets.authorize()
 # Open a sheet from a spreadsheet in one go
-sh  = gc.open_by_key("13MDkXxt9pSbPWtqJK0DXHH81kTCjg2Y_1RJa7mgALCQ")
+sh  = client.open_by_key("13MDkXxt9pSbPWtqJK0DXHH81kTCjg2Y_1RJa7mgALCQ")
 
 # Select worksheet by index. Worksheet indexes start from zero:
-wks = sh.get_worksheet(2)
+wks = sh[2]
 
-# Creating a Worksheet
-# worksheet = sh.add_worksheet(title="data", rows="1000", cols="52")
 
-print(sh.sheet1.get('A1:D11'))
+# open a worksheet as in the first example.
 
-worksheet_list = sh.worksheets()
+header = wks.cell('A1')
+header.value = 'Names'
+header.text_format['bold'] = True # make the header bold
+header.update()
 
-print(worksheet_list)
-print(wks)
+# The same can be achieved in one line
+wks.cell('B1').set_text_format('bold', True).value = 'heights'
+
+# set the names
+wks.update_values('A2:A5',[['name1'],['name2'],['name3'],['name4']])
+
+#print(sh.sheet1.get('A1:D11'))
+
+#worksheet_list = sh.worksheets()
+
+#print(worksheet_list)
